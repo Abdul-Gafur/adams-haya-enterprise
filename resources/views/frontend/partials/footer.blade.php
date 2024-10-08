9b987aef8624038fcd8b35a72f9981d0e9ba1d54
Abdul-Gafur <abdulgafurshaattir@gmail.com>
2024-10-08 18:17:46 +0000

    footer modified

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -2,12 +2,13 @@
 <footer id="footer" class="footer">
     <div class="container">
         <div class="row">
-            <div class="footer-info col-lg-4">
+            <div class="footer-info col-lg-6">
                 <h2>Adams Haya</h2>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut tempora cupiditate ducimus, soluta quasi recusandae dolorem quibusdam! Nam illo odio, porro magni ratione quaerat eveniet impedit ad, ex facere praesentium?</p>
             </div>
         
             <div class="footer-links col-lg-4">
+            <h2>Quick Links</h2>
                 <ul>
                     <li><a href="#">About Us</a></li>
                     <li><a href="#">Our Team</a></li>
@@ -17,8 +18,9 @@
                 </ul>
             </div>
         
-            <div class="footer-socials col-lg-4">
-                <ul>
+            <div class="footer-socials col-lg-2">
+                <h2>Socials</h2>
+                <ul >
                     <li><a href="#"><i class="bi bi-facebook"></i></a></li>
                     <li><a href="#"><i class="bi bi-twitter"></i></a></li>
                     <li><a href="#"><i class="bi bi-instagram"></i></a></li>
