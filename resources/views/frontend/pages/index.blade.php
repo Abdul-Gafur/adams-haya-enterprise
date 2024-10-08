3ea06dc0a3f0459a48b0886520ee0613502962e5
Abdul-Gafur <abdulgafurshaattir@gmail.com>
2024-10-08 13:20:26 +0000

    trucks section added

--- resources/views/frontend/pages/index.blade.php
+++ resources/views/frontend/pages/index.blade.php
@@ -47,6 +47,7 @@
 
     {{-- HOMEPAGE VIDEO SECTION ENDS --}}
 
+    {{-- PARNERS SECTION START --}}
     <section class="partners">
         <div class="container">
             <div class="section-title">
@@ -66,6 +67,54 @@
                 <div class="partner-item col-lg-4 d-flex justify-content-center">
                     <a href="#" target="_blank"><img src="{{ asset('assets/images/partners/moe.png') }}"
                             alt="" class="img-fluid"></a>
+                </div>
+            </div>
+        </div>
+    </section>
+    {{-- PARNERS SECTION ENDS --}}
+
+    <section class="trucks">
+        <div class="container">
+            <div class="section-title">
+                <h2>Featured Trucks</h2>
+            </div>
+            <div class="row">
+                <div class="col-lg-6 py-4">
+                    <div class="truck d-flex">
+                        <div class="truck-details">
+                            <h2>Tipper Truck</h2>
+                            <p>Tipper trucks are designed to meet the demands of construction projects. They are
+                                efficient, reliable and easy to operate.</p>
+                        </div>
+                        <img src="{{ asset('assets/images/trucks/tipper.png') }}" alt="" class="img-fluid truck-img">
+                    </div>
+                </div>
+                <div class="col-lg-6 py-4">
+                    <div class="truck d-flex">
+                        <div class="truck-details">
+                            <h2>Tankers</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim harum veritatis dolore ex, provident necessitatibus assumenda quia eveniet, voluptas mollitia repellat alias inventore nostrum nulla aliquid molestiae consequatur consectetur amet.</p>
+                        </div>
+                        <img src="{{ asset('assets/images/trucks/tanker.png') }}" alt="" class="img-fluid truck-img">
+                    </div>
+                    </div>
+                <div class="col-lg-6 py-4">
+                    <div class="truck d-flex">
+                        <div class="truck-details">
+                            <h2>Wheeler Dump Truck</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit autem ut cum porro dolores sequi ex nulla odit, perspiciatis sint earum incidunt deleniti sed veritatis laudantium nisi! Perspiciatis, assumenda earum!</p>
+                        </div>
+                        <img src="{{ asset('assets/images/trucks/wheeler.png') }}" alt="" class="img-fluid truck-img">
+                    </div>
+                </div>
+                <div class="col-lg-6 py-4">
+                    <div class="truck d-flex">
+                        <div class="truck-details">
+                            <h2>Tipper Truck</h2>
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore quis soluta provident. Repudiandae, reiciendis? Nostrum facilis deleniti iusto id ab omnis vitae illo inventore, magni, possimus vero reprehenderit minus ipsa!</p>
+                        </div>
+                        <img src="{{ asset('assets/images/trucks/tipper.png') }}" alt="" class="img-fluid truck-img">
+                    </div>
                 </div>
             </div>
         </div>
